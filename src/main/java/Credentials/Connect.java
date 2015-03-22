8752b665266164820a2a32ed9b0d8a6dca04abae
Vicky Patel <vicky.7676@yahoo.in>
2015-03-22 02:22:58 -0300

    Database Connection with GetResult in Credential

--- src/main/java/Credentials/Connect.java
+++ src/main/java/Credentials/Connect.java
@@ -8,7 +8,12 @@ package Credentials;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 
 /**
  *
@@ -29,6 +34,31 @@ public class Connect {
             ex.getMessage();
         }
         return conn;
+    }
+    
+     public static JsonArray getResults(String sql, String... params) {
+        JsonArray json = null;
+        try {
+            Connection conn = Connect.getConnection();
+            System.out.println("hello");
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setString(i + 1, params[i]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+
+            JsonArrayBuilder array = Json.createArrayBuilder();
+            while (rs.next()) {
+                array.add(Json.createObjectBuilder()
+                        .add("id", rs.getInt("id"))
+                        .add("name", rs.getString("name"))
+                );
+            }
+            json = array.build();
+        } catch (SQLException ex) {
+           // Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return json;
     }
     
 }
