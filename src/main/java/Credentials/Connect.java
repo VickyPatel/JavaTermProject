a6be774a43221f4e6925b30f5b5d54390a7fe11c
Vicky Patel <vicky.7676@yahoo.in>
2015-03-22 01:46:58 -0300

    Database Connection Check

---
+++ src/main/java/Credentials/Connect.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Credentials;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author c0633648
+ */
+public class Connect {
+    
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String jdbc = "jdbc:mysql://" + System.getenv("OPENSHIFT_MYSQL_DB_HOST") + ":" +
+                    System.getenv("OPENSHIFT_MYSQL_DB_PORT") + "/practiceblog";
+            String user = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+            String pass = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");        
+            conn = DriverManager.getConnection(jdbc, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.getMessage();
+        }
+        return conn;
+    }
+    
+}
---
+++ src/main/java/webServices/Person.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package webServices;
+import Credentials.Connect;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author c0633648
+ */
+@Path("/advertise")
+public class Person {
+    
+    @GET
+    @Produces("application/json")
+    public Response get() {
+      return Response.ok(getResults("SELECT * FROM questions")).build();
+    }
+    
+     public static JsonArray getResults(String sql, String... params) {
+        JsonArray json = null;
+        try {
+            Connection conn = Connect.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setString(i + 1, params[i]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+
+            JsonArrayBuilder array = Json.createArrayBuilder();
+            while (rs.next()) {
+                array.add(Json.createObjectBuilder()
+                        .add("id", rs.getInt("id"))
+                        .add("question", rs.getString("question"))
+                );
+            }
+            json = array.build();
+        } catch (SQLException ex) {
+           // Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return json;
+    }
+}
