f24fb24f9a2b0fe13a592e559842916c8793682a
Vicky Patel <vicky.7676@yahoo.in>
2015-03-24 15:15:17 -0400

    database check

--- src/main/java/Credentials/Connect.java
+++ src/main/java/Credentials/Connect.java
@@ -8,12 +8,8 @@ package Credentials;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
+
 
 /**
  *
@@ -26,7 +22,7 @@ public class Connect {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             String jdbc = "jdbc:mysql://" + System.getenv("OPENSHIFT_MYSQL_DB_HOST") + ":" +
-                    System.getenv("OPENSHIFT_MYSQL_DB_PORT") + "/practiceblog";
+                    System.getenv("OPENSHIFT_MYSQL_DB_PORT") + "/java";
             String user = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
             String pass = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");        
             conn = DriverManager.getConnection(jdbc, user, pass);
@@ -36,29 +32,6 @@ public class Connect {
         return conn;
     }
     
-     public static JsonArray getResults(String sql, String... params) {
-        JsonArray json = null;
-        try {
-            Connection conn = getConnection();
-            System.out.println("hello");
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            for (int i = 0; i < params.length; i++) {
-                pstmt.setString(i + 1, params[i]);
-            }
-            ResultSet rs = pstmt.executeQuery();
-
-            JsonArrayBuilder array = Json.createArrayBuilder();
-            while (rs.next()) {
-                array.add(Json.createObjectBuilder()
-                        .add("id", rs.getInt("id"))
-                        .add("name", rs.getString("name"))
-                );
-            }
-            json = array.build();
-        } catch (SQLException ex) {
-           // Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return json;
-    }
+    
     
 }
